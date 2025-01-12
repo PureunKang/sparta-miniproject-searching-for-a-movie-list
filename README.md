3407e0f1ad1b950eb696d89351891277ee6781df
PureunKang <pureun0000@gmail.com>
2025-01-13 00:54:03 +0900

    - feat : 검색 고도화 ( 실시간 검색, 엔터키 검색, 국문/영문 통합 검색 )

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # sparta-miniproject-searching-for-a-movie-list
 
-## 참고링크
+#### 참고링크
 
-https://velog.io/@ncookie/IMDB-TMDB-API-사용법-정리
-https://velog.io/@code_angler/파비콘favicon-적용이-안되시나요-파비콘-만들기-하기쉬운-오류삽질-방지
-https://velog.io/@onejuice98/사파리에서-Tab에서-Favicon-적용-안되는-이슈
-https://developer.themoviedb.org/reference/movie-details
-https://aotoyae.tistory.com/entry/JS-내배캠-영화-검색-사이트-TMDB-API
+- https://velog.io/@ncookie/IMDB-TMDB-API-사용법-정리
+- https://velog.io/@code_angler/파비콘favicon-적용이-안되시나요-파비콘-만들기-하기쉬운-오류삽질-방지
+- https://velog.io/@onejuice98/사파리에서-Tab에서-Favicon-적용-안되는-이슈
+- https://developer.themoviedb.org/reference/movie-details
+- https://aotoyae.tistory.com/entry/JS-내배캠-영화-검색-사이트-TMDB-API
+- https://velog.io/@hidaehyunlee/ES2020-click-enter-두-개의-이벤트-동시에-등록하기
+- https://velog.io/@o1011/JavaScript-디바운싱과-쓰로틀링
+
+---
+
+#### 개선 필요사항
+
+- 포스터 이미지 없을 때 처리
+- 검색 결과 출력시 검색 필드 상단으로
--- javasc.js
+++ javasc.js
@@ -8,7 +8,11 @@ const options = {
 };
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w185";
+const MOVIE_BASE_URL = "https://api.themoviedb.org/3/search/movie";
+
 const $cardContainer = document.querySelector(".card-container");
+const $targetMovie = document.querySelector("#target-movie");
+const $btn = document.querySelector('input[type="button"]');
 
 let page = 1;
 let isLoading = false;
@@ -47,6 +51,61 @@ const fetchPopularMovies = function (page) {
     });
 };
 
+// 검색을 위한 API 호출 함수
+// 한국어, 영어 검색 API 통합
+const searchMovies = function (page) {
+  const movieName = $targetMovie.value.trim();
+  if (!movieName) return;
+
+  // 기존 데이터 초기화
+  $cardContainer.innerHTML = "";
+  isLoading = true; // 스크롤 이벤트 방지
+
+  let totalResults = [];
+  let totalPages = 1;
+
+  const fetchAllPages = (language) => {
+    const fetchPage = (page) => {
+      return fetch(
+        `${MOVIE_BASE_URL}?query=${encodeURIComponent(
+          movieName
+        )}&include_adult=true&language=${language}&page=${page}`,
+        options
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          totalPages = data.total_pages;
+          totalResults = [...totalResults, ...data.results];
+        })
+        .catch((err) => console.error(err));
+    };
+
+    const promises = [];
+    for (let i = 1; i <= totalPages; i++) {
+      promises.push(fetchPage(i));
+    }
+
+    return Promise.all(promises);
+  };
+  Promise.all([fetchAllPages("ko-KR"), fetchAllPages("en-US")])
+    .then(() => {
+      // 중복된 json 데이터 제거
+      const uniqueResults = totalResults.reduce((acc, current) => {
+        if (!acc.some((movie) => movie.id === current.id)) {
+          acc.push(current);
+        }
+        return acc;
+      }, []);
+
+      // 화면에 렌더링
+      renderMovies(uniqueResults);
+    })
+    .catch((err) => {
+      console.error(err);
+      isLoading = false;
+    });
+};
+
 // 새로고침되자마자 바로 실행
 document.addEventListener("DOMContentLoaded", () => {
   fetchPopularMovies(page); // 첫 번째 페이지 데이터를 로드
@@ -61,4 +120,29 @@ window.addEventListener("scroll", function () {
     page++;
     fetchPopularMovies(page); // ++된 페이지의 데이터 요청
   }
+});
+
+// 검색 버튼 클릭 이벤트
+$btn.addEventListener("click", function () {
+  searchMovies();
+});
+
+// Enter키로 검색 버튼 대체하기 이벤트
+$targetMovie.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    searchMovies();
+  }
+});
+
+// 실시간 검색 - 디바운싱
+let timer;
+$targetMovie.addEventListener("input", function () {
+  const movieName2 = $targetMovie.value.trim();
+
+  clearTimeout(timer);
+  timer = setTimeout(function () {
+    if (movieName2) {
+      searchMovies();
+    }
+  }, 500);
 });
